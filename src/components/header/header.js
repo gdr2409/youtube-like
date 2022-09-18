394f5a8468f638ad2e5087872c1fb7b875c1ec52
Grimm_reaper (Shikhar Sharma 2) <shikhar.sharma2@shipsy.io>
2022-09-18 12:48:17 +0530

    2022-09-18: Added divs in icons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,24 +3,34 @@ import './header.css';
 
 import { Input } from 'antd';
 import { VideoCameraAddOutlined, BellOutlined, UserOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 const { Search } = Input;
 
 
+const addVideoClicked = function () {
+	alert('Add Video Clicked');
+}
+
 function Header() {
 	return (
-		<div className='header'>
-			<div className='header-logo'> {/*Logo div*/}
-				Youtube
+			<div className='header'>
+				<div className='header-logo'> {/*Logo div*/}
+					Youtube
+				</div>
+				<div className='header-search'> {/*Search bar div*/}
+					<Search placeholder='Search' />
+				</div>
+				<div className='header-extra'> {/*Extra div*/}
+					<div
+						className='header-extra-action'
+						onClick={addVideoClicked}
+					>
+						<VideoCameraAddOutlined />
+					</div>
+					<div className='header-extra-action'><BellOutlined /></div>
+					<div className='header-extra-action'><UserOutlined /></div>
+				</div>
 			</div>
-			<div className='header-search'> {/*Search bar div*/}
-				<Search placeholder='Search' />
-			</div>
-			<div className='header-extra'> {/*Extra div*/}
-				<VideoCameraAddOutlined />
-				<BellOutlined />
-				<UserOutlined />
-			</div>
-		</div>
 	);
 }
 
