3c308381bc9737f2d722b6b811628c3d7c203275
Grimm_reaper (Shikhar Sharma 2) <shikhar.sharma2@shipsy.io>
2022-09-18 18:05:35 +0530

    2022-09-18: Added youtube player

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,12 +1,33 @@
 
+import CustomLayout from '../layout/layout';
 import './player.css';
+
+import YouTube from 'react-youtube';
 
 
 function Player(props) {
 
+	const opts = {
+		width: '70%',
+		height: '480'
+	};
+
+	function _onReady(e) {
+		// access to player in all event handlers via event.target
+		e.target.pauseVideo();
+	}
+
 	return (
-		<div className='player'>
-		</div>
+		<>
+			<CustomLayout />
+			<div className='player-parent'>
+				<div className='youtube-player-parent'>
+					<div className='youtube-player'>
+						<YouTube videoId='2g811Eo7K8U' opts={opts} onReady={_onReady} />
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
 
